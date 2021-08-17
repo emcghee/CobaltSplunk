dc41eeb150c0b2f41979f467e8876b8b7fa5f280
emcghee <emcghee@vt.edu>
2021-08-17 10:35:39 -0400

    Uncommented lines after testing and added full path to executables

--- LinuxForwarder/install-forwarder.sh
+++ LinuxForwarder/install-forwarder.sh
@@ -9,17 +9,17 @@ SERVER="$1"
 PATH="$2"
 HOSTNAME="$3"
 
-# sudo dpkg -i splunkforwarder-8.2.1-ddff1c41e5cf-linux-2.6-amd64.deb 
+/usr/bin/sudo /usr/bin/dpkg -i splunkforwarder-8.2.1-ddff1c41e5cf-linux-2.6-amd64.deb
 
-# sudo /opt/splunkforwarder/bin/splunk set servername $HOSTNAME
-# sudo /opt/splunkforwarder/bin/splunk set default-hostname $HOSTNAME
+/usr/bin/sudo /opt/splunkforwarder/bin/splunk set servername $HOSTNAME
+/usr/bin/sudo /opt/splunkforwarder/bin/splunk set default-hostname $HOSTNAME
 
-# sudo /opt/splunkforwarder/bin/splunk enable boot-start
-# sudo /opt/splunkforwarder/bin/splunk add forward-server $SERVER:9997
-# sudo /opt/splunkforwarder/bin/splunk start
-# sudo /opt/splunkforwarder/bin/splunk add monitor /var/log/auth.log -index ssh -sourcetype %APP%
-# sudo /opt/splunkforwarder/bin/splunk add monitor $PATH/.../weblog_*.log -index cobalt -sourcetype weblog
-# sudo /opt/splunkforwarder/bin/splunk add monitor $PATH/.../beacon_*.log -index cobalt -sourcetype beacon_log
-# sudo /opt/splunkforwarder/bin/splunk add monitor $PATH/.../keystrokes_* -index cobalt -sourcetype keystrokes
-# sudo /opt/splunkforwarder/bin/splunk add monitor $PATH/.../events.log -index cobalt -sourcetype teamserver_events
-# sudo /opt/splunkforwarder/bin/splunk add monitor $PATH/.../screenshots.log -index cobalt -sourcetype screenshots
+/usr/bin/sudo /opt/splunkforwarder/bin/splunk enable boot-start
+/usr/bin/sudo /opt/splunkforwarder/bin/splunk add forward-server $SERVER:9997
+/usr/bin/sudo /opt/splunkforwarder/bin/splunk start
+/usr/bin/sudo /opt/splunkforwarder/bin/splunk add monitor /var/log/auth.log -index ssh -sourcetype %APP%
+/usr/bin/sudo /opt/splunkforwarder/bin/splunk add monitor $PATH/.../weblog_*.log -index cobalt -sourcetype weblog
+/usr/bin/sudo /opt/splunkforwarder/bin/splunk add monitor $PATH/.../beacon_*.log -index cobalt -sourcetype beacon_log
+/usr/bin/sudo /opt/splunkforwarder/bin/splunk add monitor $PATH/.../keystrokes_* -index cobalt -sourcetype keystrokes
+/usr/bin/sudo /opt/splunkforwarder/bin/splunk add monitor $PATH/.../events.log -index cobalt -sourcetype teamserver_events
+/usr/bin/sudo /opt/splunkforwarder/bin/splunk add monitor $PATH/.../screenshots.log -index cobalt -sourcetype screenshots
